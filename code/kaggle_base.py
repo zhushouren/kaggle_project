72c6fd6dd28258ae1a87b0ac1df3e30984206f1d
Yuquan.Zhu <2678100074@qq.com>
2018-06-02 14:20:01 +0800

    company first commit

--- code/kaggle_base.py
+++ code/kaggle_base.py
@@ -1,3 +1,15 @@
 __author__ = 'yuquan'
 import os
-import pandas 
+import pandas as pd
+
+class KaggleBase(object):
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def fit(self):
+        pass
+
+    
